ce33246e71918ff7c2213c1659513c87619922e7
Thivet <simonthivet@outlook.fr>
2018-09-25 16:10:46 +0200

    initialisation class modèle / connexion bdd

--- PPE3_genieco_v2/Program.cs
+++ PPE3_genieco_v2/Program.cs
@@ -16,7 +16,8 @@ namespace PPE3_genieco_v2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Modele.init();
+            Application.Run(new FConnex());
         }
     }
 }
