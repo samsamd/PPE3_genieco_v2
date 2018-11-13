d22195fd21edc49f72d7b77b45137344206f5420
btssio <btssio@ST201>
2018-11-13 16:08:03 +0100

    Modif 13/11 16:07

--- PPE3_genieco_v2/FRapportVisite.cs
+++ PPE3_genieco_v2/FRapportVisite.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PPE3_genieco_v2
 {
@@ -15,6 +16,38 @@ namespace PPE3_genieco_v2
         public FRapportVisite()
         {
             InitializeComponent();
+            Modele.connexionTest();
+            bsRapport.DataSource = Modele.rapportParVisiteur(Modele.VisiteurConnecte.idVisiteur);
+            lblUser.Text = Modele.VisiteurConnecte.nom + " " + Modele.VisiteurConnecte.prenom;
+            cboNumRapp.DataSource = bsRapport.DataSource;
+            cboNumRapp.DisplayMember = "idRapport";
+            cboNumRapp.ValueMember = "idRapport";
+            txtPracticien.Text = ((RAPPORT)bsRapport.Current).MEDECIN.nom.ToString();
+            txtBoxMotif.Text = ((RAPPORT)bsRapport.Current).MOTIF.libMotif.ToString();
+            txtBoxDate.Text = ((RAPPORT)bsRapport.Current).dateRapport.ToString();
+            txtBoxBilan.Text = ((RAPPORT)bsRapport.Current).bilan.ToString();
         }
+        private void FRapportVisite_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void cboNumRapp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bsRapport2.DataSource = Modele.rapportParId(int.Parse(cboNumRapp.SelectedValue.ToString()));
+            txtPracticien.Text = ((RAPPORT)bsRapport2.Current).MEDECIN.nom.ToString() ;
+            txtBoxMotif.Text = ((RAPPORT)bsRapport2.Current).MOTIF.libMotif.ToString();
+            txtBoxDate.Text = ((RAPPORT)bsRapport2.Current).dateRapport.ToString();
+            txtBoxBilan.Text = ((RAPPORT)bsRapport2.Current).bilan.ToString();
+
+        }
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
--- PPE3_genieco_v2/Modele.cs
+++ PPE3_genieco_v2/Modele.cs
@@ -4,15 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.Data.SqlClient;
 
 namespace PPE3_genieco_v2
 {
    public static class Modele
     {
         private static genico_bdd_gsbEntities maConnexion;
+
+        private static Visiteur visiteurConnecte;
+        
+
+        public static Visiteur VisiteurConnecte { get => visiteurConnecte; set => visiteurConnecte = value; }
+        
+
         public static void init()
         {
             maConnexion = new genico_bdd_gsbEntities();
+
+        }
+
+        public static void connexionTest()
+        {
+            visiteurConnecte = maConnexion.Visiteur.Where(x => x.idVisiteur == "a13").ToList()[0];
+        }
+
+        public static List<RAPPORT> rapportParVisiteur(string idVisiteur)
+        {
+            var LQuery = maConnexion.RAPPORT.ToList().Where(x => x.idVisiteur == idVisiteur);
+            return LQuery.ToList() ;
+        }
+
+        public static List<Visiteur> ListVisiteur()
+        {
+            return maConnexion.Visiteur.ToList();
+        }
+
+        public static object rapportParId(int unIdRapport)
+        {
+            var LQuery = maConnexion.RAPPORT.ToList()
+                           .Where(x => x.idRapport == unIdRapport);
+            return LQuery.ToList();
 
         }
     }
