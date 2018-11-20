00ba693ae1909da668f9ad915f886c78dda46d9a
Defosse Samuel <samuel.defosse44@gmail.com>
2018-11-20 15:21:29 +0100

    modif modele

--- PPE3_genieco_v2/Modele.cs
+++ PPE3_genieco_v2/Modele.cs
@@ -48,12 +48,59 @@ namespace PPE3_genieco_v2
 
         }
 
+
         public static List<OFFRIR> medicamentParRapport(int unIdRapport)
         {
             var LQuery = maConnexion.OFFRIR.ToList().Where(x => x.idRapport == unIdRapport);
             return LQuery.ToList();
 
         }
+
+
+       
+           
+            public static List<Visiteur> listeLesVisiteurs()
+            {
+                return maConnexion.Visiteur.ToList();
+            }
+            private static string GetMd5Hash(string PasswdSaisi)
+
+            {
+
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(PasswdSaisi);
+
+                byte[] hash = (MD5.Create()).ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < hash.Length; i++)
+
+                {
+
+                    sb.Append(hash[i].ToString("x2"));
+
+                }
+
+                return sb.ToString();
+
+            }
+            public static bool comparerMDP(string id, string mdp)
+            {
+                bool retour = false;
+                List<Visiteur> LesVisiteurs = Modele.listeLesVisiteurs();
+                string mdpmd5 = GetMd5Hash(mdp);
+                foreach (Visiteur visiteurconnecte in LesVisiteurs)
+                {
+                    if ((visiteurconnecte.identifiant == id) && (visiteurconnecte.password.Substring(2).Equals(mdpmd5, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        retour = true;
+                        break;
+                    }
+                }
+                return retour;
+            }
+
+        
 
     }
 }
